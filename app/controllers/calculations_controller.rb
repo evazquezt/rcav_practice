899a1f9647f033a6c0947f0d954c9c5aa8156f60
evazquezt <evazquez@chicagobooth.edu>
2015-05-09 17:07:38 -0500

    completed

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,10 +1,29 @@
 class CalculationsController < ApplicationController
+
   def instructions
+
   end
 
   def sqrt
+    @number = params[:id].to_i
+    @sqrt= Math.sqrt(@number)
+    render('sqrt.html.erb')
+  end
+
+  def square
+    @number = params[:id].to_i
+    @square = @number * @number
+    render('square.html.erb')
   end
 
   def pmt
+
+    @apr = params[:apr].to_i
+    @years = params[:years].to_i
+    @principal = params[:principal].to_i
+
+    @monthly_payment = (@principal*@apr/100 + @principal)/(@years*12)
   end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:id", { :controller => "calculations", :action => "sqrt" })
+
+  get("/square/:id", { :controller => "calculations", :action => "square" })
+
+  get("/payment/:apr/:years/:principal", { :controller => "calculations", :action => "pmt" })
 end
